a8cf05c593e78c2efa1838fe28decb5b84f89746
Hack Reactor Students <github@hackreactor.com>
2013-09-30 19:32:52 -0700

    Pseudoclassical implementation of subclass and superclass is passing (.toggle() is working)

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,11 +1,5 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
-  // this = Object.create(BlinkyDancer.prototype);
-  Dancer.call(this, arguments);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  // this._oldStep = Dancer.prototype.step;    // should refer to prototype's .step()
+  Dancer.apply(this, arguments);
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
@@ -16,9 +10,6 @@ BlinkyDancer.prototype.step = function(){
   /* toggle() is a jQuery method to show/hide the <span> tag.
    * See http://api.jquery.com/category/effects/ for this and
    * other effects you can use on a jQuery-wrapped html tag. */
-  Dancer.prototype.step.call(this);
+  Dancer.prototype.step.call(this, this._timeBetweenSteps);
   this.$node.toggle();
 };
-
-// BlinkyDancer.prototype = makeDancer();
-// BlinkyDancer.prototype.constructor = BlinkyDancer;
--- src/dancer.js
+++ src/dancer.js
@@ -2,16 +2,17 @@
 var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step();
+  this._timeBetweenSteps = timeBetweenSteps;
+  this.step(this._timeBetweenSteps);
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
 };
 
-Dancer.prototype.step = function() {
+Dancer.prototype.step = function(timeBetweenSteps) {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this), timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
