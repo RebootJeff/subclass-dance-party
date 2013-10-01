a1b8d86d01c06c6efc8a6f85883c6226c7adcc17
Hack Reactor Students <github@hackreactor.com>
2013-09-30 17:20:45 -0700

    Pseudoclassical pattern for BlinkyDancer is now working with functional superclass makeDancer()

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,25 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  // this = Object.create(BlinkyDancer.prototype);
+  var _dancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-
-    /* toggle() is a jQuery method to show/hide the <span> tag.
-     * See http://api.jquery.com/category/effects/ for this and
-     * other effects you can use on a jQuery-wrapped html tag. */
-    blinkyDancer.$node.toggle();
+  _dancer._oldStep = _dancer.step;
+  var self = this;
+  _dancer.step = function(){
+    _dancer._oldStep();
+    self.step();
   };
-
-  return blinkyDancer;
+  this.$node = _dancer.$node;
 };
+
+BlinkyDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  /* toggle() is a jQuery method to show/hide the <span> tag.
+   * See http://api.jquery.com/category/effects/ for this and
+   * other effects you can use on a jQuery-wrapped html tag. */
+  this.$node.toggle();
+};
+
+// BlinkyDancer.prototype = makeDancer();
+// BlinkyDancer.prototype.constructor = BlinkyDancer;
